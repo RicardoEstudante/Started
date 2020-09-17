03fb47e5e99cb0e8896bdf99c58dce3a0abd688d
RicardoEstudante <ricardo2010carvalho@hotmail.com>
2020-09-17 13:50:27 -0500

    add class arq

--- ES6/main.js
+++ ES6/main.js
@@ -1,11 +1,53 @@
-alert('Teste');
-
-class Teste {
-    metodo(){
-
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    metodo2() {
-        
+    add(data) {
+        this.data.push(data);
+        console.log(this.data)
     }
 }
+class TodoList extends List{
+    constructor() {
+        super();
+
+        this.usuario = "Ricardo";
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add("Novo todo");
+}
+
+MinhaLista.mostraUsuario();
+
+
+// ---Errado---
+// class TodoList2 {
+//     constructor() {
+//         this.todos = [];
+//     }
+
+//     static addTodo(){
+//         this.todos.push("Novo todo");
+//         console.log(this.todos);
+//     }
+// }
+// TodoList.addTodo();
+// TodoList.addTodo();
+// TodoList.addTodo();
+
+class Matematica {
+    static soma(a, b) {
+        return a + b;
+    }
+}
+
+console.log(Matematica.soma(2, 2))

585ee995f90a0e59a9f1cd20814a31d26890755c
RicardoEstudante <ricardo2010carvalho@hotmail.com>
2020-09-25 11:17:26 -0500

    searching products from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import Header from './components/Header';
+import Main from './pages/Main'
 
 import './style.css'
 
 const App = () => ( 
   <div className="App">
     <Header />
+    <Main />
   </div>
 )
   

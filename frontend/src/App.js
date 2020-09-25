d6e3eeb5176e2452d80e4fc7252cb49d5d4be268
RicardoEstudante <ricardo2010carvalho@hotmail.com>
2020-09-25 14:03:26 -0500

    config navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Routes from './routes';
 
 import Header from './components/Header';
-import Main from './pages/Main'
 
 import './style.css'
 
 const App = () => ( 
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 )
   
